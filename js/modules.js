29df9053ab42a81cee779f521b0376e52597bec6
abhradipg <42342400+abhradipg@users.noreply.github.com>
2020-08-03 01:40:09 +0530

    Add files via upload

--- js/modules.js
+++ js/modules.js
@@ -4,30 +4,48 @@ var expand_button = document.getElementById("expand");
 var collapse_button = document.getElementById("collapse");
 var expand_div = document.getElementsByClassName("head1_e");
 var collapse_div = document.getElementsByClassName("head1_c");
+var expanded=false;
 expand_div[0].style.display="none";
 
 collapse_button.addEventListener("click",function(evt){
     collapse_div[0].style.display="none";
     expand_div[0].style.display="block";
-    document.getElementById("mainBar").style.height="2px";
-    document.getElementById("myBar").style.height="2px";
-    document.getElementById("body").style.marginTop="101px";
+    document.getElementById("mainBar").style.height="0.125rem";
+    document.getElementById("myBar").style.height="0.125rem";
+    document.getElementById("body").style.marginTop="6.1325rem";
     document.getElementById("module").style.display="none";
-    document.getElementById("menu").style.marginTop="10px";
-    document.getElementById("expand").style.marginTop="10px";
-    document.getElementById("collapse").style.marginTop="10px";
+    document.getElementById("menu").style.marginTop="0.625rem";
+    document.getElementById("expand").style.marginTop="0.625rem";
+    document.getElementById("collapse").style.marginTop="0.625rem";
+    document.getElementById("navigation").style.top="4.7rem";
+    myFunction();
 });
 
 expand_button.addEventListener("click",function(evt){
     expand_div[0].style.display="none";
     collapse_div[0].style.display="block";
-    document.getElementById("mainBar").style.height="4px";
-    document.getElementById("myBar").style.height="4px"; 
-    document.getElementById("body").style.marginTop="148px";
+    document.getElementById("mainBar").style.height="0.25rem";
+    document.getElementById("myBar").style.height="0.25rem"; 
+    document.getElementById("body").style.marginTop="9.25rem";
     document.getElementById("module").style.display="block";
-    document.getElementById("menu").style.marginTop="35px";
-    document.getElementById("expand").style.marginTop="35px";
-    document.getElementById("collapse").style.marginTop="35px";
+    document.getElementById("menu").style.marginTop="2.1875rem";
+    document.getElementById("expand").style.marginTop="2.1875rem";
+    document.getElementById("collapse").style.marginTop="2.1875rem";
+    document.getElementById("navigation").style.top="7.75rem";
+    myFunction();
+});
+
+document.getElementById("menu").addEventListener("click",function(evt){
+   if (expanded==false)
+    {
+       document.getElementById("navigation").style.display="block";
+       expanded=true;
+     }
+   else
+    {
+       document.getElementById("navigation").style.display="none";
+       expanded=false;     
+     }
 });
 };
 
